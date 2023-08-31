f2a6e8510ff04f549d78c0d2f9fbc9c7df99519a
Eddie Kao <eddie@5xcampus.com>
2023-08-31 11:18:52 +0800

    remove all default components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+console.log(App);
+createApp(App).mount("#app");
