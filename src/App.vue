c9784653ae60ef73a00a33d879ed5fba689fb43b
Eddie Kao <eddie@5xcampus.com>
2023-08-31 11:59:55 +0800

    render devices

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,14 @@ export default {
       devices: [],
     };
   },
+  methods: {
+    addDevice() {
+      if (this.device !== "") {
+        this.devices.unshift(this.device);
+        this.device = "";
+      }
+    },
+  },
 };
 </script>
 
@@ -16,12 +24,16 @@ export default {
     <div>
       <label>設備名稱</label><br />
       <input type="text" v-model="device" />
-      <button>新增</button>
+      <button @click.prevent="addDevice">新增</button>
     </div>
   </form>
   <section>
     <h3 v-if="devices.length == 0">目前無任何設備</h3>
     <h3 v-else>目前共計 {{ devices.length }} 項設備</h3>
+
+    <ul>
+      <li v-for="d in devices">{{ d }}</li>
+    </ul>
   </section>
 </template>
 
