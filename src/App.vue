36a4c900ebc8530d078df0f28396bbd388cd91be
Eddie Kao <eddie@5xcampus.com>
2023-08-31 11:47:18 +0800

    add devices

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
-<script></script>
+<script>
+export default {
+  data: function () {
+    return {
+      title: "AUO",
+      device: "",
+      devices: [],
+    };
+  },
+};
+</script>
 
 <template>
-  <h1>AUO 設備管理系統</h1>
+  <h1>{{ title }} 設備管理系統</h1>
   <form>
     <div>
-      <label for="">設備名稱</label><br />
-      <input type="text" />
+      <label>設備名稱</label><br />
+      <input type="text" v-model="device" />
       <button>新增</button>
     </div>
   </form>
-  <section></section>
+  <section>
+    <h3 v-if="devices.length == 0">目前無任何設備</h3>
+    <h3 v-else>目前共計 {{ devices.length }} 項設備</h3>
+  </section>
 </template>
 
 <style scoped>
