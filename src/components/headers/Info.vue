7c247e0c3039b21e3ef72d65d0d7febc10f802c1
Eddie Kao <eddie@5xcampus.com>
2023-09-07 09:46:34 +0800

    add computed method

--- src/components/headers/Info.vue
+++ src/components/headers/Info.vue
@@ -7,10 +7,19 @@ export default {
       default: [],
     },
   },
+  computed: {
+    deviceCount() {
+      return this.devices.length;
+    },
+
+    noDevice() {
+      return this.deviceCount === 0;
+    },
+  },
 };
 </script>
 
 <template>
-  <h3 v-if="devices.length == 0">目前無任何設備</h3>
-  <h3 v-else>目前共計 {{ devices.length }} 項設備</h3>
+  <h3 v-if="noDevice">目前無任何設備</h3>
+  <h3 v-else>目前共計 {{ deviceCount }} 項設備</h3>
 </template>
